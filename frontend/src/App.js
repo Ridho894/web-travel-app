c61135ef85f07104f6ff1fba3d1c2d8792cab751
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-14 02:19:08 +0700

    mapbox last for today

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import ReactMapGL from 'react-map-gl'
-
+import ReactMapGL, { Marker } from 'react-map-gl'
+import { Room } from '@material-ui/icons'
 function App() {
   const [viewport, setViewPort] = useState({
     width: "100vw",
@@ -15,7 +15,12 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={nextViewPort => setViewPort(nextViewPort)}
-      />
+        mapStyle="mapbox://styles/kakid/cktj12at33wxg17r4paxf0mxq"
+      >
+        <Marker latitude={48.858093} longitude={2.294694} offsetLeft={-20} offsetTop={-10}>
+          <Room style={{ fontSize: viewport.zoom * 7 }} />
+        </Marker>
+      </ReactMapGL>
     </div>
   );
 }
