f9f9bd01250e61f058439749eaf06c3c88303ee7
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-14 11:28:40 +0700

    mapbox filter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import ReactMapGL, { Marker } from 'react-map-gl'
 import { Room } from '@material-ui/icons'
+
 function App() {
   const [viewport, setViewPort] = useState({
     width: "100vw",
     height: "100vh",
     latitude: 46,
     longitude: 17,
-    zoom: 4
+    zoom: 4,
   });
   return (
     <div className="App">
@@ -15,7 +16,8 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={nextViewPort => setViewPort(nextViewPort)}
-        mapStyle="mapbox://styles/kakid/cktj12at33wxg17r4paxf0mxq"
+        mapStyle="mapbox://styles/kakid/cktjkddoh03bm17qw87bini5l"
+        minZoom={4}
       >
         <Marker latitude={48.858093} longitude={2.294694} offsetLeft={-20} offsetTop={-10}>
           <Room style={{ fontSize: viewport.zoom * 7 }} />
