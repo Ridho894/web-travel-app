dcc08538f363dbeb750ff84979adec61fef9ab36
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-15 06:10:34 +0700

    add tailwind configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import ReactMapGL, { Marker } from 'react-map-gl'
+import ReactMapGL, { Marker, Popup } from 'react-map-gl'
 import { Room } from '@material-ui/icons'
 
 function App() {
@@ -16,12 +16,23 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={nextViewPort => setViewPort(nextViewPort)}
-        mapStyle="mapbox://styles/kakid/cktjkddoh03bm17qw87bini5l"
+        mapStyle="mapbox://styles/kakid/cktkmqmiz87ti17n70uh8nyv8"
         minZoom={4}
       >
         <Marker latitude={48.858093} longitude={2.294694} offsetLeft={-20} offsetTop={-10}>
           <Room style={{ fontSize: viewport.zoom * 7 }} />
         </Marker>
+        <Popup
+          latitude={48.858093}
+          longitude={2.294694}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left"
+        >
+          <div>
+            <label>Here</label>
+          </div>
+        </Popup>
       </ReactMapGL>
     </div>
   );
